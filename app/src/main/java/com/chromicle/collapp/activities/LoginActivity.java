e62287fd6f23bd3a3db34f0265d004b0f6051256
chromicle <ajayprabhakar369@gmail.com>
2020-01-25 21:48:02 +0530

    Add UI for add new book to database

--- app/src/main/java/com/chromicle/collapp/activities/LoginActivity.java
+++ app/src/main/java/com/chromicle/collapp/activities/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.chromicle.collapp;
+package com.chromicle.collapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.chromicle.collapp.R;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
